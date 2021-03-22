3e98149d5e89614919c199790cbf90ade31bbb2b
imithu <imithu@imithu.com>
2021-03-22 21:05:34 +0600

    fixed and improved withdraw methods

--- Poisa/Withdraw.php
+++ Poisa/Withdraw.php
@@ -4,6 +4,7 @@ namespace Poisa;
 
 use Illuminate\Support\Facades\DB;
 use Poisa\Balance;
+use Poisa\Setting\Profit\Withdraw as Profit_Withdraw;
 
 
 class Withdraw
@@ -15,26 +16,36 @@ class Withdraw
    * @param float $amount
    * 
    * @since   1.1.0
-   * @version 1.1.0
+   * @version 1.3.0
    * @author  Mahmudul Hasan Mithu
    */
   public static function request_manual( int $user_id, float $amount )
   {
-      date_default_timezone_set('UTC');
-      $amount_current = Balance::amount($user_id);
+    date_default_timezone_set('UTC');
+    $amount = round($amount, 2);
+    $amount_current = Balance::amount($user_id);
+    
+    if($amount<=$amount_current){
+      $admin_percent  = Profit_Withdraw::get();
+      $admin_will_get = round(($amount*$admin_percent)/100, 2);
+      $user_will_get  = $amount-$admin_will_get;
 
-      if($amount<=$amount_current){
-        Balance::change( $user_id, -$amount );
-        DB::table('Poisa_Withdraw')->insert(
-          [
-            'user_id' => $user_id,
-            'amount' => $amount,
-            'status' => 'request',
-            'method' => 'manual',
-            'datetime' => date('Y-m-d H:i:s')
-          ]
-        );
-      }
+      Balance::change( $user_id, -$amount );
+      DB::table('Poisa_Withdraw')->insert(
+        [
+          'user_id' => $user_id,
+
+          'amount' => $amount,
+          'admin_percent' => $admin_percent,
+          'user_will_get' => $user_will_get,
+          'admin_will_get' => $admin_will_get,
+
+          'status' => 'request',
+          'method' => 'manual',
+          'datetime' => date('Y-m-d H:i:s')
+        ]
+      );
+    }
   }
 
 
@@ -46,19 +57,23 @@ class Withdraw
    * @param int   $id
    * 
    * @since   1.1.0
-   * @version 1.1.0
+   * @version 1.3.0
    * @author  Mahmudul Hasan Mithu
    */
   public static function reject( int $id )
   {
-    $reject_amount = (float) DB::table('Poisa_Withdraw')->where('id', $id)->value('amount');
     $reject_user_id = (int) DB::table('Poisa_Withdraw')->where('id', $id)->value('user_id');
-    DB::table('Poisa_Withdraw')->where('id', $id)->update(
-      [
-        'method'=>'reject'
-      ]
-    );
-    Balance::change( $reject_user_id, $reject_amount );
+    $reject_amount  = (float) DB::table('Poisa_Withdraw')->where('id', $id)->value('amount');
+    $reject_status  = DB::table('Poisa_Withdraw')->where('id', $id)->value('status');
+    
+    if($reject_user_id>0 && $reject_status==='request'){
+      DB::table('Poisa_Withdraw')->where('id', $id)->update(
+        [
+          'status'=>'reject'
+        ]
+      );
+      Balance::change( $reject_user_id, $reject_amount );
+    }
   }
 
 
@@ -70,14 +85,14 @@ class Withdraw
    * @param int   $id
    * 
    * @since   1.1.0
-   * @version 1.1.0
+   * @version 1.3.0
    * @author  Mahmudul Hasan Mithu
    */
   public static function accept( int $id )
   {
     DB::table('Poisa_Withdraw')->where('id', $id)->update(
       [
-        'method'=>'accept'
+        'status'=>'accept'
       ]
     );
   }
